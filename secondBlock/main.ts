f91ee4ea42bbc8998826b6f878f76621ab35a242
quarentyne <peresada1997@gmail.com>
2022-04-18 13:53:36 +0300

    second block

--- secondBlock/main.ts
+++ secondBlock/main.ts
@@ -51,3 +51,59 @@ Function.prototype.myBind = function (context, ...rest) {
     return result;
   }
 };
+
+interface Array<T> {
+  myFilter(callback: (value: T, index: number, array: Array<T>) => boolean): Array<T>;
+  myMap(callback: (value: T, index: number, array: Array<T>) => T): Array<T>;
+  myReduce(callback: (prevValue: T, currentValue: T,
+    index: number, array: Array<T>) => T, accumulator?: T): T;
+  myFind(callback: (value: T, index: number, array: Array<T>) => boolean): T | undefined;
+  myForEach(callback: (value: T, index: number, array: Array<T>) => void): void;
+}
+
+Array.prototype.myFilter = function (callback) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i])
+    }
+  }
+  return result;
+}
+
+Array.prototype.myMap = function (callback) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
+};
+
+Array.prototype.myReduce = function (callback, initialValue) {
+  if (this.length === 0) {
+    throw new Error('reduce of empty array with no initial value');
+  }
+  let accumulator = initialValue || 0;
+  for (let i = 0; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
+  }
+  return accumulator;
+};
+
+Array.prototype.myFind = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return this[i];
+    }
+  }
+  return undefined;
+};
+
+Array.prototype.myForEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === undefined) {
+      continue;
+    }
+    callback(this[i], i, this);
+  }
+};
